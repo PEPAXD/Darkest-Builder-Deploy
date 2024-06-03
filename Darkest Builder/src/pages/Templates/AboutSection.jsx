292f6029cc8f840a699ace922db37ff07657e20e
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-06-03 17:03:31 -0300

    mainContainerAbout... desktop/mobile

--- Darkest Builder/src/pages/Templates/AboutSection.jsx
+++ Darkest Builder/src/pages/Templates/AboutSection.jsx
@@ -9,8 +9,19 @@ function AboutSection({ url_Bg }) {
         style={{ background: `url(${url_Bg}) no-repeat center top/cover` }}
       >
         <div className="AboutContainer">
-          <h1>About Commit</h1>
-          <h2>New Branch</h2>
+          <article className="aboutContain">
+            <h1>About Commit</h1>
+            <h2>New Branch</h2>
+          </article>
+
+          <div className="PointSections">
+            <span>A</span>
+            <span>B</span>
+            <span>C</span>
+            <span>D</span>
+            <span>E</span>
+            <span>F</span>
+          </div>
         </div>
       </div>
     </div>
