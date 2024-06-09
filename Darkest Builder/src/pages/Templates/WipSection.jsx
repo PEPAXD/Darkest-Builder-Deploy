b94aaa3624d819a607e87dad7d36b726a27a7f31
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-06-09 17:13:51 -0300

    wipSection devNote

--- Darkest Builder/src/pages/Templates/WipSection.jsx
+++ Darkest Builder/src/pages/Templates/WipSection.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./styles/WipSection.css";
 
-function WipSection({ url_Bg }) {
+//import RepoGithUB ICon
+import { FaGithub } from "react-icons/fa";
 
+function WipSection({ url_Bg }) {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 480);
   const backgroundIMG = [url_Bg, "wip_responsive.jpg"];
 
@@ -11,11 +13,11 @@ function WipSection({ url_Bg }) {
       setIsMobile(window.innerWidth <= 480);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     // Limpiar el evento al desmontar el componente
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -23,12 +25,46 @@ function WipSection({ url_Bg }) {
     <div className="Wip">
       <div
         className="background"
-        style={{ background: `url(${isMobile ? backgroundIMG[1] : backgroundIMG[0]}) no-repeat center top/cover` }}
+        style={{
+          background: `url(${
+            isMobile ? backgroundIMG[1] : backgroundIMG[0]
+          }) no-repeat center top/cover`,
+        }}
       >
         <div className="wipContainer">
           <div className="wipContain">
-            <h1>Work in Progress</h1>
-            <p>Sorry, this section is still under construction.</p>
+            <div className="tittleWip">
+              <h1>Work in Progress</h1>
+              <hr />
+            </div>
+            <div className="noteDev">
+              <p>
+                Due to the magnitude of the project and the limitations of time
+                and resources, I have decided to conclude the project.
+              </p>
+              <p>
+                I reached the main goal, and I am satisfied with the current
+                state of the project. However, I had some interesting ideas for
+                future updates focused on the different regions of the game,
+                enemies, builds, tier lists... and a section dedicated to the
+                fandom.
+              </p>
+              <p>
+                If any of you are interested in collaborating and helping to
+                implement these ideas or other improvements, you can do so
+                through the repository.
+              </p>
+            </div>
+
+            <a
+              href="https://github.com/PEPAXD/Darkest-Builder-Deploy"
+              target="_blank"
+            >
+              <div className="repodarkestBuilder">
+                <FaGithub />
+                <span>LinkGithub</span>
+              </div>
+            </a>
           </div>
         </div>
       </div>
