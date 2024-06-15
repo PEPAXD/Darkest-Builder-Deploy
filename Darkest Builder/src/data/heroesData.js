cffff09df089cfe988ba9d3179516ff2012ba2f7
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-06-15 19:00:23 -0300

    finish description database

--- Darkest Builder/src/data/heroesData.js
+++ Darkest Builder/src/data/heroesData.js
@@ -67,7 +67,7 @@ const heroesData = {
       "Let's keep this melody moving...",
     ],
     paths: ["Wanderer", "Virtuoso", "Soloist", "Intermezzo"],
-    description: "The Jester is a particularly mobile Hero, as most offensive skills of his skillset require him to change position around the team composition to unleash his full potential. Due to his rather erratic movements, the Jester may present risk when fit into a team who requires Rank consistency or lacks flexibility in movement. Notably, the Jester excels at applying Combo Token, possessing 4 skills that can inflict said Token in total, more than any other Heroes. Due to his highly flexible skillset with a good target reach, the Jester can reliably perform on any Rank, especially on middle Ranks. Jester has a more supporting role on Rank 3 where he can effectively Stress heal, buff his team or Pull targets, while on Rank 2 he can use Finale ability to deal a strong blow on resilient targets.",
+    description: "The Jester is a highly mobile hero with skills that require him to move around the team. His versatility allows him to apply Combo Tokens effectively across different ranks. He excels at stress healing and buffing teammates on Rank 3, while using Finale on Rank 2 for powerful attacks against tough enemies.",
   },
 
   4: {
@@ -80,7 +80,7 @@ const heroesData = {
       "To rule, one must learn to serve.",
     ],
     paths: ["Wanderer", "Tempest", "Poet", "Monarch"],
-    description: "The leper is a durable front-rank hero capable of dealing huge amounts of damage. While also being able to receive large amounts HP damage and stress for the team, protecting his allies. The main problem the player will see is Blind tokens. Fortunately Leper has the tools to combat this as  ability Reflection allows you to clear Blind. Leper main damage abilities are able to ignore Blind if you have Combo. The Leper's biggest downside is that he's restricted to the front two ranks. He has no way of attack from the back, and he has no ability that moves him forward, forcing him to move one rank at a time with his regular movement. This means enemies like Pit Fighter or The Lord are a big threat to Leper since these enemies can push him back with strong Knockback abilites.",
+    description: "The Leper is a strong frontline hero dealing high damage and absorbing damage for the team. His main weakness is Blind tokens, countered by Reflection. Limited to the front ranks, he lacks mobility and is vulnerable to knockbacks.",
   },
 
   5: {
@@ -93,6 +93,7 @@ const heroesData = {
       "Look death in the eye.",
     ],
     paths: ["Wanderer", "Sergeant", "Bulwark", "Vanguard"],
+    description: "The Man-at-Arms is essentially a tank who takes punishments to protect his more fragile teammates thanks to his easy access to Taunt and Block. Man-at-Arms can be effective at dealing damage as well.His entire skillset is highly flexible; allowing him to adapt to any Rank or Party. Because of his toughness and ability to protect his allies the Man-at-Arms can become the backbone of any team composition.",
   },
 
   6: {
@@ -105,6 +106,7 @@ const heroesData = {
       "I drift in the starless skies of the void.",
     ],
     paths: ["Wanderer", "Ritualist", "Warlock", "Aspirant"],
+    description: "the Occultist focuses on applying Negative Tokens to enemies and healing critically injured allies. As players progress, they unlock offensive skills that utilize Unchecked Power, enabling the Occultist to deal significant damage and apply powerful effects. However, harnessing Unchecked Power also introduces subtle risks for the Occultist.",
   },
 
   7: {
@@ -117,6 +119,7 @@ const heroesData = {
       "Let me prescribe a more ...holistic regimen.",
     ],
     paths: ["Wanderer", "Surgeon", "Alchemist", "Physician"],
+    description: "The Plague Doctor is a reliable healer and supportive-damage dealer, with utility, diversity, and usefulness with her skills. The Plague Doctor is capable of dealing a surprisingly massive amount of Blight damage, with some respectable Direct and Bleed damage. Due to her lack of defensive skills and low-HP. The Plague Doctor is best utilized in the back Ranks, where she has access to most of her skills, and avoids harm from the dangerous front Ranks.",
   },
 
   8: {
@@ -129,6 +132,7 @@ const heroesData = {
       "Witness ye the holy radiance of the Light!",
     ],
     paths: ["Wanderer", "Confessor", "Chaplain", "Seraph"],
+    description: "The Vestal is a versatile hero capable of supporting the team and dealing damage. Her strategic use of Conviction tokens and careful skill planning optimize her effectiveness across both front and back ranks.",
   },
 
   9: {
@@ -141,6 +145,7 @@ const heroesData = {
       "I have nothing, and so I have everything.",
     ],
     paths: ["Wanderer", "Arsonist", "Survivor", "Orphan"],
+    description: "As a new addition to the Hero's roster in Darkest Dungeon II, the Runaway's skillset has a heavy focus on Burn effect. The Runaway is primarily a front-liner, having access to most of her skills when placed on the front Ranks. Furthermore, she can target the back Ranks, provided that she stays on Rank 3 or 4, where she can not use various skills. She is one of the few Heroes that can use Stealth and also among the few that can heal allies.",
   },
 
   10: {
@@ -148,6 +153,7 @@ const heroesData = {
     rolePlay: [],
     cite: ["The thrill of the hunt, the promise of payment!"],
     paths: ["Professional"],
+    description: "The Bounty Hunter has a unique recruitment method through the inn's poster, costing 4 candles per region. He cannot form relationships but can experience stress and meltdowns. His pre-mastered skills offer powerful versatility despite the absence of paths or skill upgrades.",
   },
 
   11: {
@@ -160,6 +166,7 @@ const heroesData = {
       "When the wind whips violent, rally to my standard!",
     ],
     paths: ["Wanderer", "Aggressor", "Templar", "Banneret"],
+    description: "Reynauld is a frontline Crusader with high resistance to bleed and burn. His paths include Wanderer, Aggressor, Templar, and Banneret, each with a different focus. His abilities range from powerful strikes, stress healing, stuns, and area attacks with burns, adapting to different roles depending on the chosen path.",
   },
 
   12: {
@@ -172,6 +179,7 @@ const heroesData = {
       "I live for this",
     ],
     paths: ["Wanderer", "Instructice", "Antagoniste", "Intrédpide"],
+    description: "The Duelist is a highly-mobile, high-damage-dealing hero, with low HP and few defenses. In a way, it plays similar to the Grave Robber, however the Duelist has no self-healing skills which makes her a higher-risk, higher-reward bet.",
   },
 
   13: {
@@ -184,6 +192,7 @@ const heroesData = {
       "I am purged of beauty, empty of all but blight!",
     ],
     paths: ["Wanderer", "Maniac", "Exanimate", "Scourge"],
+    description: "The Flagellant is a hero with a unique Meltdown ability called 'Toxic'. He excels in high resistance to Death's Door and can never be resolute. His skills include blight attacks, self-healing at the cost of his own HP, stress and DoT transfer abilities, and team healing and protection capabilities. His three main paths (Maniac, Exanimate, Scourge) modify specific skills to focus on different playstyles and roles within the team.",
   },
 };
 
