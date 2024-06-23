04fb59328418088e75030165437c0fc826d044e7
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-06-22 23:20:29 -0300

    add icons and styles

--- Darkest Builder/src/components/boxStats.jsx
+++ Darkest Builder/src/components/boxStats.jsx
@@ -4,6 +4,17 @@ import "./styles/boxStats.css";
 //Imports IconsGallery
 import maxHp from "../assets/img/icons/Icon_HP_dd2.webp";
 import sPD from "../assets/img/icons/Icon_speed_dd2.webp";
+import back from "../assets/img/icons/backwards.webp";
+import forw from "../assets/img/icons/forward.webp";
+
+import bleed from "../assets/img/icons/Bleed_dd2.webp";
+import burn from "../assets/img/icons/Burn_dd2.webp";
+import blight from "../assets/img/icons/Blight_dd2.webp";
+import disease from "../assets/img/icons/Disease_dd2.webp";
+import move from "../assets/img/icons/Move_dd2.webp";
+import stun from "../assets/img/icons/Stun_dd2.webp";
+import debuff from "../assets/img/icons/Debuff_dd2.webp";
+import deathBlow from "../assets/img/icons/Deathsdoor_dd2.webp";
 
 function boxStats() {
   return (
@@ -31,19 +42,19 @@ function boxStats() {
               HP 35
             </li>
             <li>
-              <img src={maxHp} alt="statsIcon" />
+              <img src={sPD} alt="statsIcon" />
               SPD 5
             </li>
           </ul>
           <h4>Movement</h4>
           <ul>
             <li>
-              <img src={maxHp} alt="statsIcon" />
-              SPD 5
+              <img src={back} alt="statsIcon" />
+              Back 5
             </li>
             <li>
-              <img src={maxHp} alt="statsIcon" />
-              SPD 5
+              <img src={forw} alt="statsIcon" />
+              Forw 5
             </li>
           </ul>
         </div>
@@ -53,36 +64,36 @@ function boxStats() {
         <h4>Resistants</h4>
         <ul>
           <li>
-            <img src={maxHp} alt="statsIcon" />
-            SPD 5
+            <img src={bleed} alt="statsIcon" />
+            Bleed 5
           </li>
           <li>
-            <img src={maxHp} alt="statsIcon" />
-            SPD 5
+            <img src={move} alt="statsIcon" />
+            Move 5
           </li>
           <li>
-            <img src={maxHp} alt="statsIcon" />
-            SPD 5
+            <img src={burn} alt="statsIcon" />
+            Burn 5
           </li>
           <li>
-            <img src={maxHp} alt="statsIcon" />
-            SPD 5
+            <img src={stun} alt="statsIcon" />
+            Stun 5
           </li>
           <li>
-            <img src={maxHp} alt="statsIcon" />
-            SPD 5
+            <img src={blight} alt="statsIcon" />
+            Blight 5
           </li>
           <li>
-            <img src={maxHp} alt="statsIcon" />
-            SPD 5
+            <img src={debuff} alt="statsIcon" />
+            Debuff 5
           </li>
           <li>
-            <img src={maxHp} alt="statsIcon" />
-            SPD 5
+            <img src={disease} alt="statsIcon" />
+            Disease 5
           </li>
           <li>
-            <img src={maxHp} alt="statsIcon" />
-            SPD 5
+            <img src={deathBlow} alt="statsIcon" />
+            Deathblow 5
           </li>
         </ul>
       </div>
