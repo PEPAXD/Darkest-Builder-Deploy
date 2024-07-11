2e198cafe6b0bcc8194a21f3f4882de7e2d62533
Mauro Exequiel Pepa <mauropepa1997oficial@gmail.com>
2024-07-11 20:10:33 -0300

    fix-twoUpgradeButton

--- Darkest Builder/src/components/upgradeButton.jsx
+++ Darkest Builder/src/components/upgradeButton.jsx
@@ -6,29 +6,28 @@ function upgradeButton() {
 
   const handleOnChange = () => {
     setIsChecked(!isChecked);
+    console.log("isChecked", isChecked);
   };
 
   return (
     <div className="upgradeButton">
-      <button onClick={() => handleOnChange()}>
-        <div className="checkbox-wrapper-46">
-          <input
-            type="checkbox"
-            id="cbx-46"
-            className="inp-cbx"
-            checked={isChecked}
-            onChange={handleOnChange}
-          />
-          <label htmlFor="cbx-46" className="cbx">
-            <span>
-              <svg viewBox="0 0 12 10" height="10px" width="12px">
-                <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-              </svg>
-            </span>
-            <span>Upgraded</span>
-          </label>
-        </div>
-      </button>
+      <div className="checkbox-wrapper-46">
+        <input
+          type="checkbox"
+          id="cbx-46"
+          className="inp-cbx"
+          checked={isChecked}
+          onChange={handleOnChange}
+        />
+        <label htmlFor="cbx-46" className="cbx">
+          <span>
+            <svg viewBox="0 0 12 10" height="10px" width="12px">
+              <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+            </svg>
+          </span>
+          <span>Upgraded</span>
+        </label>
+      </div>
     </div>
   );
 }
